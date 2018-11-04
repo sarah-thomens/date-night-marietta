00075da5d3b984b8d5a9a24d553b2a72f61f5c7f
Sarah Marie Thomens <sarah.thomens@gmail.com>
2018-11-04 11:51:55 -0500

    Downloaded google-maps-react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MapContainer from './components/MapContainer';
 import './App.css';
 
 class App extends Component
@@ -6,10 +7,7 @@ class App extends Component
   render( )
 	{
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-      </div>
+				<MapContainer />
     );
   }
 }
