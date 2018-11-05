1f1f6c6e2e8021180c5458b2dd55d9e3cf0ef104
Sarah Marie Thomens <sarah.thomens@gmail.com>
2018-11-05 08:58:25 -0500

    Created rudimentary marker info windows

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,6 +3,21 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component
 {
+	state =
+	{
+		showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+	}
+
+	onMarkerClick = ( props, marker, e ) =>
+    this.setState(
+		{
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
   render( )
 	{
 		const { places } = this.props
@@ -18,17 +33,24 @@ export class MapContainer extends Component
             lng: -84.54961659999999
         }}
 			>
-			{
-				places.map( (place) =>
-				(
-					<Marker
-						name={ place.name }
-						position={ place.latLong }
-						key={ place.id }
-						style={{ color: myColor }}
-					/>
-				)
-			)}
+				{
+					places.map( (place) =>
+					(
+						<Marker
+							onClick={this.onMarkerClick}
+							name={ place.name }
+							position={ place.latLong }
+							key={ place.id }
+						/>
+					)
+				)}
+				<InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
       </Map>
     );
   }
---
+++ src/components/PlaceInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class PlaceInfo extends Component
+{
+	render( )
+	{
+		return(
+
+		)
+	}
+}
+
+export default PlaceInfo;
