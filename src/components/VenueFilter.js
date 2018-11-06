dd0ff2456605e60478cec62d2543f43cc77b7191
Sarah Marie Thomens <sarah.thomens@gmail.com>
2018-11-06 09:50:25 -0500

    Created VenueFilter Component

---
+++ src/components/VenueFilter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';																			// Imports React Library
+import PropTypes from 'prop-types';																						// Imports PropTypes
+
+export class VenueFilter extends Component
+{
+	//--Renders the VenueFilter Component-----------------------------------------------------------------------
+  render( )
+	{
+		return (
+			<div className="venue-filter">
+				<h1>Venue Filter<h2>
+			</div>
+		);
+	}
+}
