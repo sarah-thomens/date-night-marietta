cdd6043002969eedd8413cc66d01c6d71cb9ff8b
Sarah Marie Thomens <sarah.thomens@gmail.com>
2018-11-08 22:09:44 -0500

    Added Venue Information to ListView

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -3,12 +3,31 @@ import PropTypes from 'prop-types';																						// Imports PropTypes
 
 export class ListView extends Component
 {
+	//--PropTypes for ListView Component------------------------------------------------------------------------
+	static propTypes =
+	{
+		venues: PropTypes.array.isRequired		// venues array
+	}
+
 	//--Renders the VenueFilter Component-----------------------------------------------------------------------
   render( )
 	{
 		return (
 			<div className="list">
 				<h1>List View</h1>
+				<ul className="venues-list">
+					{ this.props.venues.map( (venue) => (
+						<li key={venue.id} className={venue.category}>
+							<div className="venue-name">
+								{venue.name}
+							</div>
+							<div className="venue-address">
+								{venue.address}
+								<span className="venue-cost"> - {venue.price}</span>
+							</div>
+						</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
